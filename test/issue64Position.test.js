d1d6d2ce4dea92195e8162b2aabbf1c56717d7a7
mlaa4ml <mlaa4ml@gmail.com>
2026-09-25 09:30:47 +0300

    test/issue64Position.test.js: patch

--- test/issue64Position.test.js
+++ test/issue64Position.test.js
@@ -4,13 +4,13 @@ import { DurakGame } from '../src/game.js';
 
 describe('Issue 64 Position & Reconstruction Tests', () => {
   it('should verify basic DurakGame creation and turn structure', () => {
-    const game = new DurakGame({ players: 2, deckSize: 36 });
+    const game = new DurakGame([{ id: 'bot1', name: 'Bot 1' }, { id: 'bot2', name: 'Bot 2' }], { deckSize: 36 });
     assert.ok(game, 'Game should be created');
     assert.strictEqual(game.getState().players.length, 2);
   });
 
   it('should support endgame position simulation structure', () => {
-    const game = new DurakGame({ players: 2, deckSize: 36 });
+    const game = new DurakGame([{ id: 'bot1', name: 'Bot 1' }, { id: 'bot2', name: 'Bot 2' }], { deckSize: 36 });
     const state = game.getState();
     assert.strictEqual(typeof state.deck.length, 'number');
   });
